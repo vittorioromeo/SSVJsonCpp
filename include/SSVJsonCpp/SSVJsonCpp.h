ebc303aa4c4bb1f558413e6b819db75bec3500a0
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-06-21 16:29:31 +0200

    license updated

--- include/SSVJsonCpp/SSVJsonCpp.h
+++ include/SSVJsonCpp/SSVJsonCpp.h
@@ -1,1 +1,10 @@
+// Copyright (c) 2013 Vittorio Romeo
+// License: Academic Free License ("AFL") v. 3.0
+// AFL License page: http://opensource.org/licenses/AFL-3.0
+
+#ifndef SSVUTILSJSON
+#define SSVUTILSJSON
+
 #include "json/json.h"
+
+#endif
