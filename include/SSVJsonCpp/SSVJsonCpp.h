2b3c25cb4efb274aa2cbca8747ce4039691f7392
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-06-21 21:04:00 +0200

    fixed fatal bug, whoops.

--- include/SSVJsonCpp/SSVJsonCpp.h
+++ include/SSVJsonCpp/SSVJsonCpp.h
@@ -2,8 +2,8 @@
 // License: Academic Free License ("AFL") v. 3.0
 // AFL License page: http://opensource.org/licenses/AFL-3.0
 
-#ifndef SSVUTILSJSON
-#define SSVUTILSJSON
+#ifndef SSVJSONCPP
+#define SSVJSONCPP
 
 #include "json/json.h"
 
